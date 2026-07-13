7f0cc4fdf9ba39327524fe0b6e33c8c249586a27
CyCklop <ignacioandresflores06@gmail.com>
2026-07-12 23:22:43 -0400

    debug: añadi un bean y un valor para testear en el main

--- src/main/java/cl/esperanza/gateway/GatewayApplication.java
+++ src/main/java/cl/esperanza/gateway/GatewayApplication.java
@@ -1,15 +1,26 @@
 package cl.esperanza.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
+    
+    @Value("${mi.prueba}")
+    private String prueba;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+
+    @Bean
+    CommandLineRunner test() {
+        return args -> System.out.println("VALOR = " + prueba);
+    }
 }
